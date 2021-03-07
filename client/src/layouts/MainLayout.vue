4b334a923378f3f4e436738c59d64aec3ce0caf2
reile <reilefer17@gmail.com>
2021-03-07 11:44:06 -0400

    inicio de usuario y menu drawer

--- client/src/layouts/MainLayout.vue
+++ client/src/layouts/MainLayout.vue
@@ -2,29 +2,54 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar class="bg-white row justify-between">
-        <q-btn round dense flat icon="keyboard_backspace" color="primary" @click="$router.go(-1)"/>
-        <q-btn flat round dense :icon="rol !== 1 ? 'person' : ''" color="primary" @click="rol !== 1 ? $router.push('/Datos') : ''"  />
+        <q-btn round dense flat icon="menu" color="primary" @click="drawer = !drawer"/>
+        <div class="text-black">Inicio</div>
+        <q-btn flat dense :icon="rol !== 1 ? 'person' : ''" class="bg-secondary" @click="rol !== 1 ? $router.push('/Datos') : ''"  style=""/>
       </q-toolbar>
     </q-header>
-    <q-footer>
+    <q-drawer v-if="drawer === true" v-model="drawer" :width="200" :breakpoint="500" overlay bordered content-class="bg-white">
+      <q-separator/>
+      <q-scroll-area class="fit">
+        <q-list>
+          <template v-for="(item, index) in menu">
+            <q-item :key="index" clickable v-ripple v-if="can(item.permission)" @click="item.label === 'Cerrar Sesión' ? cerrarSesion() : $router.push(item.ruta)">
+              <q-item-section avatar>
+                <q-icon :name="item.icon" />
+              </q-item-section>
+              <q-item-section>
+                  {{ item.label }}
+              </q-item-section>
+            </q-item>
+            <q-separator :key="'sep' + index" />
+          </template>
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
+    <!-- <q-footer>
       <div class="bg-grey-1 text-primary shadow-2 full-width row justify-around" >
         <q-btn icon="home" color="primary" flat round size="md" :to="rol === 2 ? '/inicio_cliente' : rol === 3 ? '/inicio_proveedor' : rol === 1 ? '/inicio_administrador' : ''" />
         <q-btn v-if="rol != 1" :icon="rol === 3 ? 'view_list' : 'pets'" color="primary" flat round size="md" :to="rol === 2 ? '/mascotas' : rol === 3 ? '/productos' : ''"/>
-        <q-btn v-if="rol != 1" :icon="rol === 3 ? 'hotel' : 'shopping_cart'" color="primary" flat round size="md" :to="rol === 2 ? '#' : rol === 3 ? '/hospedaje' : ''"/> -->
-        <!-- <q-btn :icon="rol === 1 ? 'lock_clock' : 'shopping_cart'" color="primary" flat round size="md" :to="rol === 1 ? '/proveedores_pendientes' : '/mis_cotizaciones'" /> -->
+        <q-btn v-if="rol != 1" :icon="rol === 3 ? 'hotel' : 'shopping_cart'" color="primary" flat round size="md" :to="rol === 2 ? '#' : rol === 3 ? '/hospedaje' : ''"/>
+        <q-btn :icon="rol === 1 ? 'lock_clock' : 'shopping_cart'" color="primary" flat round size="md" :to="rol === 1 ? '/proveedores_pendientes' : '/mis_cotizaciones'" />
         <q-btn icon="logout" color="primary" flat round size="md" to="/login" />
       </div>
-    </q-footer>
+    </q-footer> -->
     <q-page-container>
       <router-view />
+      <q-page-sticky position="bottom-right" :offset="[18, 18]">
+        <q-btn v-if="rol != 1 && 3" round icon="shopping_bag" color="primary" size="20px">
+          <q-badge color="red" label="2" floating/>
+        </q-btn>
+      </q-page-sticky>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-
+import { mapGetters } from 'vuex'
 export default {
   computed: {
+    ...mapGetters('generals', ['can']),
     mostrarBoton () {
       return this.$route.meta.botonchat
     }
@@ -33,7 +58,28 @@ export default {
   data () {
     return {
       usuario: {},
-      rol: null
+      rol: null,
+      drawer: false,
+      menu: [
+        {
+          icon: 'home',
+          label: 'Inicio',
+          ruta: '/inicio',
+          permission: 1
+        },
+        {
+          icon: 'pets',
+          label: 'Mascotas',
+          ruta: '/mascotas',
+          permission: 1
+        },
+        {
+          icon: 'logout',
+          label: 'Cerrar Sesión',
+          ruta: '',
+          permission: 1
+        }
+      ]
     }
   },
   mounted () {
--- client/src/pages/cliente/Inicio.vue
+++ client/src/pages/cliente/Inicio.vue
@@ -1,6 +1,259 @@
 <template>
-  <div>
-    <div class="q-mt-lg text-center q-pa-md text-weight-bolder bg-secondary text-black q-ma-md" style="border-radius:12px">BIENVENIDO CLIENTE</div>
+  <div style="max-width: 100%">
+    <div class="q-mt-md q-mx-sm text-h6">Bienvenido Usuario</div>
+    <div class="q-mb-md q-mx-sm text-secondary text-weight-bolder">El amor es una palabra de cuatro patas</div>
+    <q-input outlined type="text" class="q-mb-xs q-mx-sm" label="Que necesitas?" dense style="width: auto border-radius: 20px">
+      <template v-slot:prepend>
+        <q-icon name="search"/>
+      </template>
+    </q-input>
+    <div class="q-mb-md q-mx-sm text-h6">Catalogo de productos</div>
+    <q-scroll-area horizontal class="q-mb-md" style="height: 48px;">
+      <q-tabs v-model="tab" dense class="text-grey" active-color="primary" indicator-color="primary" align="justify" narrow-indicator>
+        <q-tab label="Categoria" />
+        <q-tab label="Categoria" />
+        <q-tab label="Categoria" />
+        <q-tab label="Categoria" />
+      </q-tabs>
+    </q-scroll-area>
+    <div class="q-mb-xs q-mx-sm">Ultimos Productos agregados</div>
+    <q-scroll-area horizontal class="q-mx-sm q-mb-md" style="height: 320px;">
+      <div class="row no-wrap" style="width: 100%">
+        <q-card class="q-mt-xs q-mr-sm bordes" style="width: 200px; height: 300px;">
+          <q-card-section style="height: 65%;">
+            <q-avatar square class="absolute-center" color="secondary" style="width: 180px; height: 90%"/>
+          </q-card-section>
+          <q-separator />
+          <q-card-section class="bg-orange-2" style="height: 35%;">
+            <div>
+              <div class="text-subtitle2" style="font-size: 13px">Nombre del producto</div>
+              <div class="items-center row text-grey">
+                  <q-icon name="place" />
+                  <div class="text-subtitle2" style="font-size: 12px">Tienda</div>
+              </div>
+            </div>
+            <div class="items-center row justify-between">
+              <div class="row">
+                <q-icon size="20px" name="star" color="secondary" v-for="(i, index) in 5" :key="index"/>
+              </div>
+              <q-btn round icon="keyboard_arrow_right" text-color="primary" color="white" size="10px"/>
+            </div>
+          </q-card-section>
+        </q-card>
+        <q-card class="q-mt-xs q-mr-sm bordes" style="width: 200px; height: 300px;">
+          <q-card-section style="height: 65%;">
+            <q-avatar square class="absolute-center" color="secondary" style="width: 180px; height: 90%"/>
+          </q-card-section>
+          <q-separator />
+          <q-card-section class="bg-orange-2" style="height: 35%;">
+            <div>
+              <div class="text-subtitle2" style="font-size: 13px">Nombre del producto</div>
+              <div class="items-center row text-grey">
+                  <q-icon name="place" />
+                  <div class="text-subtitle2" style="font-size: 12px">Tienda</div>
+              </div>
+            </div>
+            <div class="items-center row justify-between">
+              <div class="row">
+                <q-icon size="20px" name="star" color="secondary" v-for="(i, index) in 5" :key="index"/>
+              </div>
+              <q-btn round icon="keyboard_arrow_right" text-color="primary" color="white" size="10px"/>
+            </div>
+          </q-card-section>
+        </q-card>
+        <q-card class="q-mt-xs q-mr-sm bordes" style="width: 200px; height: 300px;">
+          <q-card-section style="height: 65%;">
+            <q-avatar square class="absolute-center" color="secondary" style="width: 180px; height: 90%"/>
+          </q-card-section>
+          <q-separator />
+          <q-card-section class="bg-orange-2" style="height: 35%;">
+            <div>
+              <div class="text-subtitle2" style="font-size: 13px">Nombre del producto</div>
+              <div class="items-center row text-grey">
+                  <q-icon name="place" />
+                  <div class="text-subtitle2" style="font-size: 12px">Tienda</div>
+              </div>
+            </div>
+            <div class="items-center row justify-between">
+              <div class="row">
+                <q-icon size="20px" name="star" color="secondary" v-for="(i, index) in 5" :key="index"/>
+              </div>
+              <q-btn round icon="keyboard_arrow_right" text-color="primary" color="white" size="10px"/>
+            </div>
+          </q-card-section>
+        </q-card>
+        <q-card class="q-mt-xs q-mr-sm bordes" style="width: 200px; height: 300px;">
+          <q-card-section style="height: 65%;">
+            <q-avatar square class="absolute-center" color="secondary" style="width: 180px; height: 90%"/>
+          </q-card-section>
+          <q-separator />
+          <q-card-section class="bg-orange-2" style="height: 35%;">
+            <div>
+              <div class="text-subtitle2" style="font-size: 13px">Nombre del producto</div>
+              <div class="items-center row text-grey">
+                  <q-icon name="place" />
+                  <div class="text-subtitle2" style="font-size: 12px">Tienda</div>
+              </div>
+            </div>
+            <div class="items-center row justify-between">
+              <div class="row">
+                <q-icon size="20px" name="star" color="secondary" v-for="(i, index) in 5" :key="index"/>
+              </div>
+              <q-btn round icon="keyboard_arrow_right" text-color="primary" color="white" size="10px"/>
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </q-scroll-area>
+    <div class="q-mb-md q-mx-sm text-h6">Nuestras tiendas</div>
+    <q-scroll-area horizontal class="q-mb-md" style="height: 48px;">
+      <q-tabs v-model="tab" dense class="text-grey" active-color="primary" indicator-color="primary" align="justify" narrow-indicator>
+        <q-tab label="Servicio" />
+        <q-tab label="Servicio" />
+        <q-tab label="Servicio" />
+        <q-tab label="Servicio" />
+        <q-tab label="Servicio" />
+        <q-tab label="Servicio" />
+      </q-tabs>
+    </q-scroll-area>
+    <div class="q-mb-xs q-mx-sm">Tiendas mejores calificadas</div>
+    <q-scroll-area horizontal class="q-mx-sm q-mb-md" style="height: 320px;">
+      <div class="row no-wrap" style="width: 100%">
+        <q-card class="q-mt-xs q-mr-sm bordes" style="width: 200px; height: 300px;">
+          <q-card-section style="height: 65%;">
+            <q-avatar square class="absolute-center" color="secondary" style="width: 180px; height: 90%"/>
+          </q-card-section>
+          <q-separator />
+          <q-card-section class="bg-orange-2" style="height: 35%;">
+            <div>
+              <div class="text-subtitle2" style="font-size: 13px">Nombre de la tienda</div>
+              <div class="items-center row text-grey">
+                  <q-icon name="place" />
+                  <div class="text-subtitle2" style="font-size: 12px">Dirección de la tienda</div>
+              </div>
+            </div>
+            <div class="items-center row justify-between">
+              <div class="row">
+                <q-icon size="20px" name="star" color="secondary" v-for="(i, index) in 5" :key="index"/>
+              </div>
+              <q-btn round icon="keyboard_arrow_right" text-color="primary" color="white" size="10px"/>
+            </div>
+          </q-card-section>
+        </q-card>
+        <q-card class="q-mt-xs q-mr-sm bordes" style="width: 200px; height: 300px;">
+          <q-card-section style="height: 65%;">
+            <q-avatar square class="absolute-center" color="secondary" style="width: 180px; height: 90%"/>
+          </q-card-section>
+          <q-separator />
+          <q-card-section class="bg-orange-2" style="height: 35%;">
+            <div>
+              <div class="text-subtitle2" style="font-size: 13px">Nombre de la tienda</div>
+              <div class="items-center row text-grey">
+                  <q-icon name="place" />
+                  <div class="text-subtitle2" style="font-size: 12px">Dirección de la tienda</div>
+              </div>
+            </div>
+            <div class="items-center row justify-between">
+              <div class="row">
+                <q-icon size="20px" name="star" color="secondary" v-for="(i, index) in 5" :key="index"/>
+              </div>
+              <q-btn round icon="keyboard_arrow_right" text-color="primary" color="white" size="10px"/>
+            </div>
+          </q-card-section>
+        </q-card>
+        <q-card class="q-mt-xs q-mr-sm bordes" style="width: 200px; height: 300px;">
+          <q-card-section style="height: 65%;">
+            <q-avatar square class="absolute-center" color="secondary" style="width: 180px; height: 90%"/>
+          </q-card-section>
+          <q-separator />
+          <q-card-section class="bg-orange-2" style="height: 35%;">
+            <div>
+              <div class="text-subtitle2" style="font-size: 13px">Nombre de la tienda</div>
+              <div class="items-center row text-grey">
+                  <q-icon name="place" />
+                  <div class="text-subtitle2" style="font-size: 12px">Dirección de la tienda</div>
+              </div>
+            </div>
+            <div class="items-center row justify-between">
+              <div class="row">
+                <q-icon size="20px" name="star" color="secondary" v-for="(i, index) in 5" :key="index"/>
+              </div>
+              <q-btn round icon="keyboard_arrow_right" text-color="primary" color="white" size="10px"/>
+            </div>
+          </q-card-section>
+        </q-card>
+        <q-card class="q-mt-xs q-mr-sm bordes" style="width: 200px; height: 300px;">
+          <q-card-section style="height: 65%;">
+            <q-avatar square class="absolute-center" color="secondary" style="width: 180px; height: 90%"/>
+          </q-card-section>
+          <q-separator />
+          <q-card-section class="bg-orange-2" style="height: 35%;">
+            <div>
+              <div class="text-subtitle2" style="font-size: 13px">Nombre de la tienda</div>
+              <div class="items-center row text-grey">
+                  <q-icon name="place" />
+                  <div class="text-subtitle2" style="font-size: 12px">Dirección de la tienda</div>
+              </div>
+            </div>
+            <div class="items-center row justify-between">
+              <div class="row">
+                <q-icon size="20px" name="star" color="secondary" v-for="(i, index) in 5" :key="index"/>
+              </div>
+              <q-btn round icon="keyboard_arrow_right" text-color="primary" color="white" size="10px"/>
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </q-scroll-area>
+    <div class="q-mx-sm text-h6">Alojamientos</div>
+    <div class="q-mb-md q-mx-sm">Alojamientos mejor catificados</div>
+    <q-list class="q-px-sm q-mb-xl" style="width: 100%; height: auto;">
+      <q-card class="q-mb-md" style="width: 100%; border-radius: 12px;">
+        <q-card-section class="bg-secondary" style="height: 175px;">
+          <q-btn position="top-left" round icon="favorite" color="primary" size="10px"/>
+        </q-card-section>
+        <q-separator />
+        <q-card-section class="row justify-between">
+          <div>
+            <div class="text-subtitle2" style="font-size: 13px">Nombre del alojamiento</div>
+            <div class="items-center row text-grey">
+                <q-icon name="place" />
+                <div class="text-subtitle2" style="font-size: 12px">País, Ciudad</div>
+            </div>
+          </div>
+          <q-btn flat dense class="bg-primary text-white" style="width: 100px">$850 / mes</q-btn>
+        </q-card-section>
+      </q-card>
+      <q-card class="q-mb-md" style="width: 100%; border-radius: 12px">
+        <q-card-section class="bg-secondary" style="height: 175px;">
+          <q-btn position="top-left" round icon="favorite" color="primary" size="10px"/>
+        </q-card-section>
+        <q-separator />
+        <q-card-section class="row justify-between">
+          <div>
+            <div class="text-subtitle2" style="font-size: 13px">Nombre del alojamiento</div>
+            <div class="items-center row text-grey">
+                <q-icon name="place" />
+                <div class="text-subtitle2" style="font-size: 12px">País, Ciudad</div>
+            </div>
+          </div>
+          <q-btn flat dense class="bg-primary text-white" style="width: 100px">$850 / mes</q-btn>
+        </q-card-section>
+      </q-card>
+    </q-list>
+    <div class="column items-center" style="height: 250px;">
+      <div class="text-weight-bolder" style="font-size: 15px">Suscribete a nuestro boletín</div>
+      <div style="font-size: 11px">Recibe las mejores ofertas de todos</div>
+      <div  class="q-mb-md" style="font-size: 11px">nuestros proveedores</div>
+      <q-input outlined label="Tu correo aquí" color-label="secondary">
+        <template v-slot:prepend>
+          <q-icon name="mail_outline"/>
+        </template>
+        <template v-slot:append class="bg-secondary">
+          <q-btn flat dense icon="send" class="bg-primary" text-color="white" style="height: 100%; width: 50px; margin-right: -11px"/>
+        </template>
+      </q-input>
+    </div>
   </div>
 </template>
 
@@ -10,6 +263,7 @@ export default {
   },
   data () {
     return {
+      tab: 'mails'
     }
   },
   mounted () {
@@ -19,11 +273,10 @@ export default {
 }
 </script>
 
-<style>
-.estilo-titulos {
-  background-color: #fff599;
-  width: 250px;
-  border-radius: 12px
-}
-
-</style>>
+<style lang="sass" scoped>
+.bordes
+  border-top-left-radius: 0px
+  border-top-right-radius: 30px
+  border-bottom-left-radius: 30px
+  border-bottom-right-radius: 30px
+</style>
