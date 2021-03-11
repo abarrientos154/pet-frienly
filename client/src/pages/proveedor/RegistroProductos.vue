8ce5978a1fb24a4ca313504e19a6ba8953e8fc5d
example <example@example.com>
2021-03-10 23:14:27 -0400

    10/03/2021

--- client/src/pages/proveedor/RegistroProductos.vue
+++ client/src/pages/proveedor/RegistroProductos.vue
@@ -1,51 +1,93 @@
 <template>
   <div class="q-pa-md">
-    <div class="q-mt-sm q-mb-sm text-subtitle2 q-pl-sm">{{edit ? 'Modificar Producto' : 'Nuevo Producto'}}</div>
-    <q-input rounded outlined bg-color="yellow-2" v-model="form.name" label="Nombre" dense :error="$v.form.name.$error" error-message="Este campo es requerido"  @blur="$v.form.name.$touch()"/>
-    <div class="q-mt-lg q-mb-sm text-subtitle2 q-pl-sm">Descripcion</div>
-      <q-input rounded outlined bg-color="yellow-2" v-model="form.description" type="textarea" :error="$v.form.description.$error" error-message="Este campo es requerido"  @blur="$v.form.description.$touch()"/>
-    <div class="row">
-      <div class="q-mt-lg q-ml-sm q-mb-sm text-subtitle2">Cantidad</div>
-      <div class="q-mt-md q-ml-xl q-mb-sm">
-        <q-input bg-color="primary" v-model.number="form.cantidad" type="number" dense :error="$v.form.cantidad.$error" error-message="Este campo es requerido"  @blur="$v.form.cantidad.$touch()"/>
-      </div>
-    </div>
-    <div class="row items-center">
-      <div class="q-mt-lg q-ml-sm q-mb-sm text-subtitle2">Agregar Foto</div>
-      <div class="q-ml-md q-mt-lg row">
-        <q-file label="Click aqui" hint="Opcional" dense class="q-pa-sm" bg-color="yellow-2" style="width:200px" v-model="file" accept=".jpg, image/*" @input="changeFile" :error="$v.file.$error" error-message="Este campo es requerido"  @blur="$v.file.$touch()" >
-          <template v-slot:before>
-            <q-avatar size="60px" square>
-              <q-img :src="imgPro ? imgPro : 'noimgpro.png'" />
-            </q-avatar>
+    <q-card flat bordered>
+      <q-card-section horizontal>
+        <q-card-section>
+          <div class="column items-center justify-center">
+            <q-btn v-for="(item, index) in botones" :key="index" icon="edit" color="grey-5" :flat="!item.select" class="q-mt-xl" @click="seleccionar(index)" style="height:50px" />
+          </div>
+        </q-card-section>
+        <div class="column full-width">
+          <q-img style="height: 250px;" class="col" :src="mostrarImg">
+            <q-file borderless v-model="productoFile" class="absolute-top-right q-ma-sm button-camera" @input="perfil_img()" accept=".jpg, image/*" style="z-index:1">
+              <q-icon name="mode_edit" class="absolute-center" size="20px" color="white" />
+            </q-file>
+          </q-img>
+          <div class="q-mt-lg text-h6">Nombre del producto</div>
+          <q-input filled v-model="form.name"  label="Nombre del producto" dense :error="$v.form.name.$error" error-message="Este campo es requerido"  @blur="$v.form.name.$touch()">
+           <template v-slot:append>
+            <q-icon name="mode_edit" />
           </template>
-        </q-file>
+          </q-input>
+          <div class="q-mt-md">
+              <q-input filled label="$0.00" color="primary" v-model.number="form.precio" type="number" dense :error="$v.form.precio.$error" error-message="Este campo es requerido"  @blur="$v.form.precio.$touch()"/>
+            </div>
+          <div class="q-mt-md text-h6">Descripcion</div>
+            <q-input filled outlined v-model="form.descripcion" type="textarea" :error="$v.form.descripcion.$error" error-message="Este campo es requerido"  @blur="$v.form.descripcion.$touch()"/>
+            <div class=" row">
+        <div class="q-mt-sm text-h6 q-pr-sm">Cantidad</div>
+        <q-input v-model.number="form.cantidad" class="q-pr-sm" type="number" dense :error="$v.form.cantidad.$error" error-message=""  @blur="$v.form.cantidad.$touch()" style="width: 60px"/>
+          <div class="q-pr-xs">
+          <q-btn color="grey" icon="add" style="height: 40px;width:40px" @click="form.cantidad++"/>
+          <q-btn color="grey" icon="remove" style="height: 40px;width:40px" @click="resta()"/>
+          </div>
       </div>
-    </div>
+        </div>
+      </q-card-section>
+    </q-card>
     <div class="row justify-center q-ma-lg">
-      <q-btn color="primary" text-color="white" :label="edit ? 'Actualizar Producto' : 'Agregar Producto'" @click="!edit ? agregar() : actualizarProducto()"/>
+      <q-btn color="primary" text-color="white" rounded class="full-width" label="Guardar" @click="agregar()"/>
     </div>
   </div>
 </template>
 
-<script>
+<script></script>
 import { required, minLength, maxLength } from 'vuelidate/lib/validators'
 import env from '../../env'
 export default {
   data () {
     return {
+      mostrarImg: 'noimgpro.png',
       id: '',
+      productoFile: null,
       edit: false,
-      form: {},
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      form: {
+        cantidad: 0
+      },
       file: null,
-      imgPro: null
+      imgPro: null,
+      botones: [
+        {
+          file: null,
+          select: true
+        },
+        {
+          file: null,
+          select: false
+        },
+        {
+          file: null,
+          select: false
+        },
+        {
+          file: null,
+          select: false
+        },
+        {
+          file: null,
+          select: false
+        }
+      ]
     }
   },
   validations: {
     form: {
       name: { required, minLength: minLength(3), maxLength: maxLength(50) },
-      description: { required },
-      cantidad: { required, minLength: minLength(1), maxLength: maxLength(20) }
+      descripcion: { required },
+      cantidad: { required, minLength: minLength(1), maxLength: maxLength(20) },
+      precio: { required, minLength: minLength(1) }
+
     },
     file: { required }
   },
@@ -64,25 +106,60 @@ export default {
     }
   },
   methods: {
+    perfil_img () {
+      const indexSelect = this.botones.findIndex(v => v.select)
+      this.botones[indexSelect].file = this.productoFile
+      this.mostrarImg = URL.createObjectURL(this.productoFile)
+      this.productoFile = null
+    },
+    seleccionar (ind) {
+      const data = this.botones
+      const indexSelect = data.findIndex(v => v.select)
+      data[indexSelect].select = false
+      data[ind].select = true
+      this.botones = data
+      if (data[ind].file) {
+        this.mostrarImg = URL.createObjectURL(data[ind].file)
+      } else {
+        this.mostrarImg = 'noimgpro.png'
+      }
+    },
     changeFile () {
       if (this.file) { this.imgPro = URL.createObjectURL(this.file) }
     },
+    resta () {
+      if (this.form.cantidad > 0) {
+        this.form.cantidad = this.form.cantidad - 1
+      }
+    },
     async agregar () {
       this.$v.$touch()
-      if (!this.$v.form.$error && !this.$v.file.$error) {
+      if (!this.$v.form.$error && this.form.cantidad > 0) {
         this.$q.loading.show({
           message: 'Subiendo Producto, Por Favor Espere...'
         })
         var formData = new FormData()
-        formData.append('files', this.file)
+        var cantidadArchivos = 0
+        for (const j in this.botones) {
+          if (this.botones[j].file) {
+            cantidadArchivos++
+            formData.append('files_' + cantidadArchivos, this.botones[j].file)
+          }
+        }
+        this.form.cantidadArchivos = cantidadArchivos
         formData.append('dat', JSON.stringify(this.form))
         await this.$api.post('producto', formData, {
           headers: {
             'Content-Type': undefined
           }
         }).then((res) => {
+          this.$q.notify({
+            message: 'Producto agregado con exito',
+            color: 'primary'
+          })
           this.$q.loading.hide()
-          this.$router.push('/productos')
+
+          // this.$router.push('/productos')
         })
       }
     },
@@ -114,13 +191,14 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 .button-camera {
-  border-radius: 12px;
-  border: 2px solid $secondary;
-  width: 100px;
-  height: 50px;
-  background-color: $primary;
+  text-decoration: none;
+  padding: 10px;
+  font-weight: 540;
   font-size: 0px;
+  color: white;
+  height:40px;
+  width:40px
 }
 </style>
