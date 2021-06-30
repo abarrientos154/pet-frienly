8793b4e3064b9561e8119c7ed5c490465e9fe904
Magdy <example@example.com>
2021-06-29 20:47:14 -0400

    logo y login

--- client/src/pages/Login.vue
+++ client/src/pages/Login.vue
@@ -1,67 +1,61 @@
 <template>
-<div>
-  <div class="column justify-center items-center q-mt-xl bg-white no-box-shadow">
-          <div class="row q-pa-md justify-center items-center no-box-shadow">
-            <div class="column no-box-shadow">
-              <div class="row justify-center">
-                <q-avatar size="150px">
-                  <img src="https://cdn.quasar.dev/app-icons/icon-128x128.png" />
-                </q-avatar>
-              </div>
-              <div class="column items-center justify-center text-black text-h6"> Bienvenido de vuelta!</div>
-              <div class="col-xs-12 col-sm-6 q-ma-sm col-md-6 col-lg-6">
-                <div class="q-pl-xl q-mb-sm text-black text-caption"> Correo electronico</div>
-                <q-input dense type="email" v-model="form.email" placeholder="Correo electronico" autofocus filled :error="$v.form.email.$error" error-message="Este campo es requerido" @blur="$v.form.email.$touch()" >
-                  <template v-slot:before>
-                    <q-icon name="mail_outline" color= "secondary" />
-                  </template>
-                </q-input>
-                  <div class="q-pl-xl q-mb-sm text-black text-caption"> Contraseña</div>
-                  <q-input dense :type="isPwd ? 'password' : 'text'" v-model="form.password" placeholder="Contraseña" filled :error="$v.form.password.$error" error-message="Este campo es requerido" @blur="$v.form.password.$touch()" >
-                    <template v-slot:before>
-                    <q-icon name="vpn_key" color= "secondary" />
-                  </template>
-                  </q-input>
-              </div>
-              <div class="row justify-center">
-                <div class="text-caption text-secondary" style="cursor:pointer">¿Olvidaste tu contraseña?</div>
-              </div>
-              <div class="col-xs-12 col-sm-6 q-ma-sm col-md-6 col-lg-6">
-                <q-btn
-                  class="full-width q-py-sm"
-                  color="primary"
-                  :loading="loading"
-                  @click="onSubmit()"
-                  no-caps
-                >Ingresar
-                  <template v-slot:loading>
-                    <q-spinner-hourglass class="on-center" />
-                    Loading...
-                  </template>
-                </q-btn>
-              </div>
-              <div class="row justify-center">
-                <div class="row">
-                  <div class="text-black text-h9">Aun no tienes cuenta?</div>
-                  <div class="text-secondary q-ml-sm text-bold text-h9" style="cursor:pointer" @click="$router.push('/registro')">Registrate.</div>
-                </div>
-               </div>
-              <div class="colunm justify-center q-pa-sm">
-                <p align="center" style="cursor:pointer"><a class="text-bold text-caption text-grey">O registrate con algunas de tus redes sociales</a></p>
-                <div class="row justify-between q-mr-xl q-ml-xl q-mt-sm q-mb-sm">
-                  <q-btn  round color="grey-5" style="width: 40px; height: 40px">
-                    <img src="icons/Iconos_Redes.png" style="width: 20px; height: 20px"/>
-                  </q-btn>
-                  <q-btn  round color="grey-5" style="width: 40px; height: 40px">
-                    <img src="icons/Iconos_Redes_1.png" style="width: 20px; height: 20px"/>
-                  </q-btn>
-                  <q-btn  round color="grey-5" style="width: 40px; height: 40px">
-                    <img src="icons/Iconos_Redes_3.png" style="width: 20px; height: 20px"/>
-                  </q-btn>
-                </div>
-              </div>
-            </div>
+  <div>
+    <div class="absolute-center column justify-center items-center bg-white" style="width:100%">
+      <div class="column q-pa-md">
+        <div class="row justify-center q-pt-xl">
+          <img src="logo.png" style="width:260px" />
+        </div>
+
+        <div class="col-xs-12 col-sm-6 q-ma-sm col-md-6 col-lg-6">
+          <div class="q-pl-lg q-mb-sm text-black text-caption"> Correo electronico</div>
+          <q-input dense type="email" v-model="form.email" placeholder="Correo electronico" autofocus filled :error="$v.form.email.$error" error-message="Este campo es requerido" @blur="$v.form.email.$touch()" >
+            <template v-slot:before>
+              <q-icon name="mail_outline" color= "secondary" />
+            </template>
+          </q-input>
+          <div class="q-pl-lg q-mb-sm text-black text-caption"> Contraseña</div>
+          <q-input dense :type="isPwd ? 'password' : 'text'" v-model="form.password" placeholder="Contraseña" filled :error="$v.form.password.$error" error-message="Este campo es requerido" @blur="$v.form.password.$touch()" >
+            <template v-slot:before>
+              <q-icon name="vpn_key" color= "secondary" />
+            </template>
+          </q-input>
+        </div>
+
+        <div class="row justify-center">
+          <div class="text-caption text-secondary" style="cursor:pointer">¿Olvidaste tu contraseña?</div>
+        </div>
+        <div class="col-xs-12 col-sm-6 q-ma-sm col-md-6 col-lg-6">
+          <q-btn class="full-width q-py-sm" color="primary" :loading="loading"
+            @click="onSubmit()" no-caps
+              >Ingresar
+              <template v-slot:loading>
+                <q-spinner-hourglass class="on-center" />
+                Loading...
+              </template>
+          </q-btn>
+        </div>
+        <div class="row justify-center">
+          <div class="row">
+            <div class="text-black text-h9">Aun no tienes cuenta?</div>
+            <div class="text-secondary q-ml-sm text-bold text-h9" style="cursor:pointer" @click="$router.push('/registro')">Registrate.</div>
           </div>
+        </div>
+
+        <div class="colunm justify-center q-pa-sm">
+          <p align="center" style="cursor:pointer"><a class="text-bold text-caption text-grey">O registrate con algunas de tus redes sociales</a></p>
+          <div class="row justify-between q-mr-xl q-ml-xl q-mt-sm q-mb-sm">
+            <q-btn  round color="grey-5" style="width: 40px; height: 40px">
+              <img src="icons/Iconos_Redes.png" style="width: 20px; height: 20px"/>
+            </q-btn>
+            <q-btn  round color="grey-5" style="width: 40px; height: 40px">
+              <img src="icons/Iconos_Redes_1.png" style="width: 20px; height: 20px"/>
+            </q-btn>
+            <q-btn  round color="grey-5" style="width: 40px; height: 40px">
+              <img src="icons/Iconos_Redes_3.png" style="width: 20px; height: 20px"/>
+            </q-btn>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
