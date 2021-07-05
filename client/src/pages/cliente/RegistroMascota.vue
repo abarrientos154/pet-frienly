fa64596ea5a50a234402e55890a0071e2f9fa24f
josnielfermin <jfermin.wx@gmail.com>
2021-07-05 12:12:41 -0400

    avance creacion mascota

--- client/src/pages/cliente/RegistroMascota.vue
+++ client/src/pages/cliente/RegistroMascota.vue
@@ -1,85 +1,130 @@
 <template>
  <div>
-   <div class="column text-h6 q-my-md items-center">Nueva Mascota</div>
-   <div class="row justify-center">
-      <div class="q-pa-md col col-xs-10 col-sm-8 col-md-7 col-lg-5 col-xl-4 no-wrap q-mx-md q-my-sm">
-        <div class="column dimension no-wrap">
-          <q-input outlined bg-color="amber-2" v-model="form.name" label="Nombre" dense :error="$v.form.name.$error" error-message="Este campo es requerido"  @blur="$v.form.name.$touch()"/>
-          <q-input class="q-mt-md" outlined autogrow bg-color="amber-2" v-model="form.race" label="Raza" dense :error="$v.form.race.$error" error-message="Este campo es requerido" @blur="$v.form.race.$touch()" />
-          <q-input class="q-mt-md" outlined autogrow bg-color="amber-2" v-model="form.age" label="Edad" dense :error="$v.form.age.$error" error-message="Este campo es requerido" @blur="$v.form.age.$touch()" />
-        </div>
-        <div class="dimension no-wrap">
-          <q-input class="q-mt-md" outlined autogrow bg-color="amber-2" v-model="form.vaccines" label="Vacunas" dense :error="$v.form.vaccines.$error" error-message="Este campo es requerido" @blur="$v.form.vaccines.$touch()" />
-          <q-input class="q-mt-md" outlined autogrow bg-color="amber-2" v-model="form.diseases" label="Enfermedades" dense :error="$v.form.diseases.$error" error-message="Este campo es requerido" @blur="$v.form.diseases.$touch()" />
-        </div>
+   <div class="q-pa-lg">
+      <q-btn flat rounded color="primary" icon="arrow_back" @click="$router.push('/inicio_cliente')"/>
+      <q-card style="height: 150px" class="q-mx-xl q-my-xl bg-grey">
+        <q-img style="height: 100%;" :src="files[0] ? petImg[0] : ''">
+        </q-img>
+      </q-card>
+      <div class="text-center text-h5 text-grey-8">Cuentanos de tu <br> mascota</div>
+      <div style="text-align: left; padding-top: 20px">
+      <div class="text-subtitle2 q-ml-md">Fotos de tu mascota</div>
+      <div class="text-overline q-ml-md">Puede cargar hasta 3 fotos</div>
+      <div class="row justify-around q-my-md">
+        <q-avatar square size="80px">
+          <img :src="files[0] ? petImg[0] : 'petnoimg.png'" style="width: 150%">
+            <q-file borderless v-model="files[0]" class="absolute-center button-subir" @input="changePetFile(0)" accept=".jpg, image/*" style="z-index:1">
+              <q-icon name="photo_camera" class="absolute-center" size="20px" color="white" />
+            </q-file>
+        </q-avatar>
+        <q-avatar square size="80px">
+          <img :src="files[1] ? petImg[1] : 'petnoimg.png'" style="width: 150%">
+            <q-file borderless v-model="files[1]" class="absolute-center button-subir" @input="changePetFile(1)" accept=".jpg, image/*" style="z-index:1">
+              <q-icon name="photo_camera" class="absolute-center" size="20px" color="white" />
+            </q-file>
+        </q-avatar>
+        <q-avatar square size="80px">
+          <img :src="files[2] ? petImg[2] : 'petnoimg.png'" style="width: 150%">
+            <q-file borderless v-model="files[2]" class="absolute-center button-subir" @input="changePetFile(2)" accept=".jpg, image/*" style="z-index:1">
+              <q-icon name="photo_camera" class="absolute-center" size="20px" color="white" />
+            </q-file>
+        </q-avatar>
       </div>
-      <div class="q-pa-md col col-xs-10 col-sm-8 col-md-7 col-lg-5 col-xl-4 no-wrap q-mx-md q-my-sm">
-        <div class="dimension no-wrap">
-          <q-card class="q-mb-md bg-amber-2" style="border-radius:10px">
-            <q-card-section>
-              <div>Albúm de fotos</div>
-              <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 row justify-between">
-              <div class="col-10">
-                  <q-file max-files="5" style="width: 100%" accept=".jpg, image/*" multiple append v-model="album" hint="Pueden ser hasta 5 fotos" outlined label="CLICK AQUÍ" :error="$v.album.$error" error-message="Este campo es requerido" @blur="$v.album.$touch()">
-                    </q-file>
-              </div>
-              <div class="col-2 row justify-center">
-                <q-icon size="md" name="close" color="negative" @click="album = [], imgMascota = [], edit ? imgsTraidas() : ''" class="cursor-pointer" />
-              </div>
-              </div>
-            </q-card-section>
-              <q-separator />
-              <q-card-section class="row justify-around">
-                <div v-if="!imgMascota.length" class="text-subtitle2 text-grey text-center">No hay fotos de la mascota</div>
-                <div v-else v-ripple v-for="(item, index) in imgMascota" :key="index" class="col-5 q-pa-sm">
-                  <q-img
-                    :src="imgMascota.length > 0 ? imgMascota[index] : 'favicon.ico'"
-                    style="width:120px"
-                  />
-                </div>
-              </q-card-section>
-          </q-card>
-          <q-card class="q-pa-md" style="border-radius:10px">
-            <div class="text-h6 q-ml-sm q-pt-xs">Descripción personal</div>
-            <q-input borderless v-model="form.personal_description" type="textarea" />
-          </q-card>
-        </div>
       </div>
-    </div>
-   <div class="column q-my-md">
-      <div class="row justify-center q-pa-sm">
-        <q-btn color="primary" :label="edit ? 'Actualizar Mascota' : 'Crear Mascota'" @click="!edit ? agregar() : actualizarMascota()"/>
+      <div>
+        <div>Nombre de mascota</div>
+        <div class="text-overline q-ml-md">Solo 25 caracteres</div>
+        <q-input filled v-model="form.name"  dense error-message="Requerido" :error="$v.form.name.$error" @blur="$v.form.name.$touch()"/>
+      </div>
+      <div>
+        <div>Edad</div>
+        <div class="text-overline q-ml-md">¿Cuántos años tiene tu mascota?</div>
+        <q-input type="number" min="0" filled v-model.number="form.age"  dense error-message="Requerido" :error="$v.form.age.$error" @blur="$v.form.age.$touch()"/>
+      </div>
+      <div>
+        <div>Fecha de nacimiento</div>
+        <div class="text-overline q-ml-md">¿Cuándo nacio tu mascota?</div>
+        <q-input filled readonly dense v-model="form.birthdate" placeholder="dd/mm/aaaa" @click="$refs.qDateProxy.show()"
+        error-message="Este campo es requerido" :error="$v.form.birthdate.$error" @blur="$v.form.birthdate.$touch()">
+          <template v-slot:append>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
+                <q-date v-model="form.birthdate" mask="DD/MM/YYYY">
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Cerrar" color="primary" flat />
+                  </div>
+                </q-date>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+      </div>
+      <div>
+        <div>¿Qué tipo de mascota tienes?</div>
+        <div class="text-overline q-ml-md">Selecciona el tipo de mascota</div>
+        <q-select filled v-model="form.type"  dense placeholder="Escoja un tipo" error-message="Requerido" :error="$v.form.type.$error" @blur="$v.form.type.$touch()" option-value="name" option-label="name" emit-value map-options :options="petType" @input="getSize(form.type)">
+      </q-select>
+      </div>
+      <div>
+        <div>Raza</div>
+        <div class="text-overline q-ml-md">¿Qué raza es tu mascota?</div>
+        <q-input type="tel" filled v-model="form.race"  dense placeholder="Escoja una raza" error-message="Requerido" :error="$v.form.race.$error" @blur="$v.form.race.$touch()"/>
+      </div>
+      <div>
+        <div>Tamaño</div>
+        <div class="text-overline q-ml-md">Tamaño de tu mascota</div>
+        <q-select filled v-model="form.size"  dense placeholder="indique el tamaño" error-message="Requerido" :error="$v.form.size.$error" @blur="$v.form.size.$touch()" option-value="name" option-label="name" emit-value map-options :options="sizes">
+      </q-select>
+      </div>
+      <div>
+        <div>Descripción</div>
+        <div class="text-overline q-ml-md">Hasta 100 caracteres</div>
+        <q-input type="textarea" filled v-model="form.description" error-message="Requerido" :error="$v.form.description.$error" @blur="$v.form.description.$touch()"/>
+      </div>
+      <div class="column items-center">
+        <q-btn rounded class="q-pa-sm" color="primary" label="Crear Mascota" style="width: 70%;" @click="savePet()"/>
       </div>
     </div>
  </div>
 </template>
 
 <script>
-import { required } from 'vuelidate/lib/validators'
+import { required, maxLength } from 'vuelidate/lib/validators'
 import env from '../../env'
 export default {
   data () {
     return {
-      perfilFile: null,
-      imgPerfil: '',
+      terms: false,
       form: {},
-      album: [],
-      imgMascota: [],
-      id: '',
-      file: null,
-      edit: false,
-      editImg: false
+      files: [null, null, null],
+      petImg: [null, null, null],
+      perfilFile: null,
+      sizeDog: [
+        { name: 'Pequeño', value: 1 },
+        { name: 'Mediano', value: 2 },
+        { name: 'Intermedio', value: 3 },
+        { name: 'Grande', value: 4 }
+      ],
+      sizeCat: [
+        { name: 'Pequeño', value: 1 },
+        { name: 'Grande', value: 4 }
+      ],
+      petType: [
+        { name: 'Perro', value: 1 },
+        { name: 'Gato', value: 2 }
+      ],
+      sizes: []
     }
   },
   validations: {
     form: {
-      name: { required },
-      race: { required },
+      name: { required, maxLength: maxLength(25) },
       age: { required },
-      vaccines: { required },
-      diseases: { required },
-      personal_description: { required }
-
+      birthdate: { required },
+      type: { required },
+      race: { required },
+      size: { required },
+      description: { required, maxLength: maxLength(100) }
     },
     album: { required }
   },
@@ -108,10 +153,46 @@ export default {
     }
   },
   methods: {
-    test () {
-      if (this.perfilFile) { this.imgPerfil = URL.createObjectURL(this.perfilFile) }
+    async savePet () {
+      this.$v.form.$touch()
+      if (!this.$v.form.$error) {
+        this.$q.loading.show({
+          message: 'Subiendo su mascota, Por Favor Espere...'
+        })
+        const formDataTwo = new FormData()
+        if (this.files) {
+          this.form.cantidadArchivos = this.files.length
+          for (let i = 0; i < this.files.length; i++) {
+            formDataTwo.append('album' + i, this.files[i])
+          }
+        } else {
+          this.form.cantidadArchivos = 0
+        }
+        formDataTwo.append('dat', JSON.stringify(this.form))
+        await this.$api.post('mascota', formDataTwo, {
+          headers: {
+            'Content-Type': undefined
+          }
+        }).then(res => {
+          if (res) {
+            this.$q.loading.hide()
+            this.$q.notify({
+              message: 'Tu mascota ha sido creada correctamente',
+              color: 'positive'
+            })
+            this.$router.push('/inicio_cliente')
+          }
+        })
+      }
     },
-    imgsTraidas () {
+    getSize (value) {
+      if (value === 'Perro') {
+        this.sizes = [...this.sizeDog]
+      } else if (value === 'Gato') {
+        this.sizes = [...this.sizeCat]
+      }
+    },
+    /* imgsTraidas () {
       for (let i = 0; i < this.form.images.length; i++) {
         console.log('prueba')
         var cc = ''
@@ -186,6 +267,9 @@ export default {
           this.$router.push('/mascotas')
         })
       }
+    }, */
+    changePetFile (ind) {
+      if (this.files[ind]) { this.petImg[ind] = URL.createObjectURL(this.files[ind]) }
     }
   }
 }
